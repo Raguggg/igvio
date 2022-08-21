7fa10320630c58866c86635c36689cde8f6cad9a
Ragu <88898517+Raguggg@users.noreply.github.com>
2022-08-21 21:53:01 +0530

    Updates

---
+++ precache-manifest.4833103ebc6b0cf46171f145e45fcf16.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f93c6eead032a9d57765",
+    "url": "/igvio/static/css/main.4e84b119.chunk.css"
+  },
+  {
+    "revision": "f93c6eead032a9d57765",
+    "url": "/igvio/static/js/main.f93c6eea.chunk.js"
+  },
+  {
+    "revision": "7146a77d249105830b25",
+    "url": "/igvio/static/css/1.2a28b150.chunk.css"
+  },
+  {
+    "revision": "7146a77d249105830b25",
+    "url": "/igvio/static/js/1.7146a77d.chunk.js"
+  },
+  {
+    "revision": "4595f0c0166675a19a2b",
+    "url": "/igvio/static/js/runtime~main.4595f0c0.js"
+  },
+  {
+    "revision": "6301297ac733b3e318b807eb55f957da",
+    "url": "/igvio/static/media/heart.6301297a.svg"
+  },
+  {
+    "revision": "38e0100174850ad63cc913ec33210613",
+    "url": "/igvio/static/media/plays.38e01001.svg"
+  },
+  {
+    "revision": "51eecdcc49042b216adf07b625828dc1",
+    "url": "/igvio/static/media/post-icon.51eecdcc.svg"
+  },
+  {
+    "revision": "97d2435d4ffaba3893e1ee71e2c15b54",
+    "url": "/igvio/static/media/reel-icon.97d2435d.svg"
+  },
+  {
+    "revision": "4bf935926e42ad1aef8f4b4a8abfde5d",
+    "url": "/igvio/static/media/story-icon.4bf93592.svg"
+  },
+  {
+    "revision": "91d2b6db0bb18aadf760c62cb3174ce7",
+    "url": "/igvio/static/media/igtv-icon.91d2b6db.svg"
+  },
+  {
+    "revision": "dc78c1727eb186d82b273f76140343a2",
+    "url": "/igvio/static/media/feed.dc78c172.svg"
+  },
+  {
+    "revision": "f3ca8bf4ff05387dc61e89fcfafec5dd",
+    "url": "/igvio/static/media/profile-icon.f3ca8bf4.svg"
+  },
+  {
+    "revision": "b92b00f5975e359c11a88cf754287b25",
+    "url": "/igvio/static/media/music.b92b00f5.png"
+  },
+  {
+    "revision": "b12a43567c1db8a2170b50e198078f2b",
+    "url": "/igvio/index.html"
+  }
+];
